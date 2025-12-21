87da88af4e07ddc5fa8082dfdbc3f32656373769
Brent Cramp <legacylanebrent@gmail.com>
2025-12-21 12:44:11 +0000

    File changes

--- src/pages/Worksheet.jsx
+++ src/pages/Worksheet.jsx
@@ -5,6 +5,7 @@ import { createPageUrl } from '@/utils';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { 
   ArrowLeft, DollarSign, Users, Building2, Package, Receipt, 
