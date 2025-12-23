35c626eb36b21dce12c2a30bdc318ba97e201ddc
Brent Cramp <legacylanebrent@gmail.com>
2025-12-23 09:23:32 +0000

    File changes

--- src/components/notifications/NotificationsDropdown.jsx
+++ src/components/notifications/NotificationsDropdown.jsx
@@ -65,7 +65,7 @@ export default function NotificationsDropdown({ user }) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon" className="relative">
+        <Button variant="ghost" size="icon" className="relative text-white hover:bg-slate-800">
           <Bell className="w-5 h-5" />
           {unreadCount > 0 && (
             <Badge className="absolute -top-1 -right-1 h-5 w-5 flex items-center justify-center p-0 bg-red-600 text-xs">
