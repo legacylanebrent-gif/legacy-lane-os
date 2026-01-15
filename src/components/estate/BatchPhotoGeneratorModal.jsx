7df74710baab7f3ecdb271efa958f453e1321e82
Brent Cramp <legacylanebrent@gmail.com>
2026-01-15 23:06:17 +0000

    File changes

--- src/components/estate/BatchPhotoGeneratorModal.jsx
+++ src/components/estate/BatchPhotoGeneratorModal.jsx
@@ -31,7 +31,6 @@ export default function BatchPhotoGeneratorModal({
     const start = currentBatchIndex;
     const end = Math.min(start + BATCH_SIZE, images.length);
     setBatch(images.slice(start, end));
-    setResults([]);
   };
 
   const generateBatch = async () => {
