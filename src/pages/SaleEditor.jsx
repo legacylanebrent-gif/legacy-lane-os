8ed380eb18bbc11cd3d1297720d3a74a4fd9a2f4
Brent Cramp <legacylanebrent@gmail.com>
2026-01-15 19:07:19 +0000

    File changes

--- src/pages/SaleEditor.jsx
+++ src/pages/SaleEditor.jsx
@@ -241,6 +241,13 @@ export default function SaleEditor() {
 
   return (
     <div className="min-h-screen bg-slate-50">
+      <Helmet>
+        <script 
+          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY || 'AIzaSyCPPGWYI7pPqvykcKlKu9G5eI_q8tquVV4'}&libraries=places`}
+          async
+          defer
+        ></script>
+      </Helmet>
       {/* Header */}
       <div className="bg-white border-b border-slate-200 sticky top-0 z-10">
         <div className="max-w-5xl mx-auto px-6 py-4 flex items-center justify-between">
