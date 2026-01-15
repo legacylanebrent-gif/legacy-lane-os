9af897e22e900bff12024c10d23062f75883bb78
Brent Cramp <legacylanebrent@gmail.com>
2026-01-15 20:11:21 +0000

    File changes

--- src/pages/MySales.jsx
+++ src/pages/MySales.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { base44 } from '@/api/base44Client';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
