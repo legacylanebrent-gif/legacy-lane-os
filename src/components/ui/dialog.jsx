4901afeef0ca9bfa74d54996807bb2024fdab13f
Brent Cramp <legacylanebrent@gmail.com>
2026-01-16 12:39:54 +0000

    File changes

--- src/components/ui/dialog.jsx
+++ src/components/ui/dialog.jsx
@@ -37,8 +37,8 @@ const DialogContent = React.forwardRef(({ className, children, ...props }, ref) 
       {...props}>
       {children}
       <DialogPrimitive.Close
-        className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
-        <X className="h-4 w-4" />
+        className="absolute right-4 top-4 bg-slate-900/80 hover:bg-slate-900 text-white rounded-full p-3 transition-all ring-offset-background focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none">
+        <X className="h-6 w-6" />
         <span className="sr-only">Close</span>
       </DialogPrimitive.Close>
     </DialogPrimitive.Content>
